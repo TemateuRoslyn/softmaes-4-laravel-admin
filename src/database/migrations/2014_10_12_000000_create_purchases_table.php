4306f4a365cf806721f7423c34db28783faabac5
theblackmamba08 <fossocabrel08@gmail.com>
2023-11-17 18:35:32 +0100

    mise à jour du fichier de migration de la table purchases

--- src/database/migrations/2014_10_12_000000_create_purchases_table.php
+++ src/database/migrations/2014_10_12_000000_create_purchases_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('purchases', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('purchase_date',50);
+            $table->string('purchase_no', 10);
+            $table->string('supplier_id', 11);
+            $table->string('purchases_status', 10);
+            $table->string('created_by', 11);
+            $table->string('updated_by', 11);
             $table->timestamps();
         });
     }
