de443da4fd176c0af04f426f8847ab0c9b9bf858
theblackmamba08 <fossocabrel08@gmail.com>
2023-11-20 20:02:24 +0100

    finalisation dans la création des fichiers de migration

---
+++ src/database/migrations/2014_10_12_000010_create_purchases_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_details', function (Blueprint $table) {
+            $table->string('purchase_id');
+            $table->string('product_id');
+            $table->integer('quantity', 11);
+            $table->integer('unitcost', 11);
+            $table->integer('total', 11);
+            $table->timestamps();
+
+            $table->foreignId('purchase_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_details');
+    }
+};
--- src/database/migrations/2014_10_12_000011_create_photos_table.php
+++ src/database/migrations/2014_10_12_000011_create_photos_table.php
@@ -16,9 +16,10 @@ return new class extends Migration
         Schema::create('photos', function (Blueprint $table) {
             $table->id();
             $table->string('photos');
-            $table->foreignId('user_id')->constrained('users');
+            $table->string('user_id');
 
-              });
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
     }
 
     /**
